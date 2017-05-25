7112093d749e98bb6c402d7fdfbe77b64b4b6b50
YogendraBG <yogendrabg@visualbi.com>
2017-05-25 04:04:59 -0500

    Conflict fix

--- src/main/java/com/visualbi/automation/pages/DualAxesChartPage.java
+++ src/main/java/com/visualbi/automation/pages/DualAxesChartPage.java
@@ -2,6 +2,9 @@ package com.visualbi.automation.pages;
 
 import com.visualbi.automation.pages.base.CustomFluentWebElementComponent;
 import com.visualbi.automation.pages.base.HighchartsPage;
+import org.fluentlenium.core.domain.FluentList;
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by Code Generation Tool on Fri Jan 13 04:23:56 CST 2017.
@@ -23,8 +26,26 @@ import com.visualbi.automation.pages.base.HighchartsPage;
 
   return $(".highcharts-plot-line-label").get(0).as(CustomFluentWebElementComponent.class);
  }
- public CustomFluentWebElementComponent yAxisPlotBand(){
-  // return yAxisPlotBand.as(CustomFluentWebElementComponent.class);
-  return $(".highcharts-plot-bands-0 .highcharts-plot-band").get(0).as(CustomFluentWebElementComponent.class);
+
+ @FindBy(css =".highcharts-series.highcharts-series-0.highcharts-column-series.highcharts-color-undefined.highcharts-tracker > rect:nth-child(1)")
+ protected FluentWebElement highchartsDataSeriesColor;
+ public CustomFluentWebElementComponent highchartsDataSeriesColor()
+ {
+  return highchartsDataSeriesColor.as(CustomFluentWebElementComponent.class);
+ }
+
+ @FindBy(css=".highcharts-legend-item.highcharts-column-series.highcharts-color-undefined.highcharts-series-0 > rect")
+ protected FluentWebElement legendItem;
+ public CustomFluentWebElementComponent legendItem(){
+  return legendItem.as(CustomFluentWebElementComponent.class);
+ }
+
+
+ @FindBy(css=".highcharts-markers.highcharts-series-1.highcharts-line-series.highcharts-color-undefined.highcharts-tracker > path:nth-child(2)")
+ protected FluentWebElement highchartsMarkerColor;
+
+ public CustomFluentWebElementComponent highchartsMarkerColor()
+ {
+  return highchartsMarkerColor.as(CustomFluentWebElementComponent.class);
  }
 }
