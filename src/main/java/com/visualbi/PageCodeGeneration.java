9da3e09050dd6a46d96507730eb27e7c40979546
vbimanonmanim <manonmanim@visualbi.com>
2017-07-12 21:00:15 +0530

    vbx141-3

--- src/main/java/com/visualbi/PageCodeGeneration.java
+++ src/main/java/com/visualbi/PageCodeGeneration.java
@@ -40,9 +40,8 @@ public class PageCodeGeneration {
                 writer.close();
             } catch (IOException e) {
                 e.printStackTrace();
-				//System.out.println(e);
-				//Svafe
-            }
+				..jhnv
+				}
 
 
         });
